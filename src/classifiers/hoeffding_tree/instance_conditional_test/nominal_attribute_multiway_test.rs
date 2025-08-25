317a33658bfa4401edb9073db3280cd8d8bfc73f
Rafael Venetikides <90425850+RafaVenetikides@users.noreply.github.com>
2025-08-25 17:29:34 -0300

    Feat: implementation of InstanceConditionalTest
    This commit closes #26 by implementing the InstanceConditionalTest trait
    and it's concrete implementations for nominal binary, nominal multiway
    and numeric binary.
    
    This commit is the first step to the implementation of the Nodes struct,
    necessary for Hoeffding Tree construction.

---
+++ src/classifiers/hoeffding_tree/instance_conditional_test/nominal_attribute_multiway_test.rs
@@ -0,0 +1,38 @@
+use crate::classifiers::hoeffding_tree::instance_conditional_test::instance_conditional_test::InstanceConditionalTest;
+use crate::core::instances::Instance;
+use std::sync::Arc;
+
+pub struct NominalAttributeMultiwayTest {
+    attribute_index: usize,
+}
+
+impl NominalAttributeMultiwayTest {
+    pub fn new(attribute_index: usize) -> Self {
+        Self { attribute_index }
+    }
+}
+
+impl InstanceConditionalTest for NominalAttributeMultiwayTest {
+    fn branch_for_instance(&self, instance: Arc<dyn Instance>) -> Option<usize> {
+        if instance
+            .is_missing_at_index(self.attribute_index)
+            .unwrap_or(true)
+        {
+            return None;
+        }
+
+        Some(instance.value_at_index(self.attribute_index)? as usize)
+    }
+
+    fn result_known_for_instance(&self, instance: Arc<dyn Instance>) -> bool {
+        self.branch_for_instance(instance).is_some_and(|b| b == 0)
+    }
+
+    fn max_branches(&self) -> usize {
+        usize::MAX
+    }
+
+    fn get_atts_test_depends_on(&self) -> Vec<usize> {
+        vec![self.attribute_index]
+    }
+}
--- src/classifiers/mod.rs
+++ src/classifiers/mod.rs
@@ -1,2 +1,3 @@
 mod attribute_class_observers;
 mod classifier;
+mod hoeffding_tree;
